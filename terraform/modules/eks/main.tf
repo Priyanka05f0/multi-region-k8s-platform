3f7b723a988b69837dd06e56610556822e75a9fd
Priyanka05f0 <lakshmipriyankabethampudi.23@gmail.com>
2026-01-16 22:40:45 +0530

    Deploy sample app and add EKS node group

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,6 @@
+########################################
+# IAM Role for EKS Cluster
+########################################
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.project_name}-${var.region}-eks-cluster-role"
 
@@ -23,6 +26,9 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+########################################
+# EKS Cluster
+########################################
 resource "aws_eks_cluster" "this" {
   name     = "${var.project_name}-${var.region}"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -38,5 +44,73 @@ resource "aws_eks_cluster" "this" {
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+########################################
+# IAM Role for Worker Nodes
+########################################
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-${var.region}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Project = var.project_name
+    Region  = var.region
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+########################################
+# EKS Node Group (Worker Nodes)
+########################################
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.project_name}-${var.region}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  tags = {
+    Project = var.project_name
+    Region  = var.region
+  }
+
+  depends_on = [
+    aws_eks_cluster.this,
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
   ]
 }
